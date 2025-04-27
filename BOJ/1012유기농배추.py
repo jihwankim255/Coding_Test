d9a75e453ec87e99f284062edc4c412397f2f4b7
jhwan <plasticat104@gmail.com>
2025-04-27 18:15:54 +0900

    유기농배추 (실2)

--- BOJ/1012유기농배추.py
+++ BOJ/1012유기농배추.py
@@ -1,5 +1,5 @@
 '''
-hf"t: {T}"tps://www.acmicpc.net/problem/1012
+https://www.acmicpc.net/problem/1012
 dfs
 테스트케이스가 머임?
 
@@ -9,12 +9,37 @@ dfs
 # n, m = map(int, input().split())
 # arr = [list(map(int, input())) for _ in range(m)]
 
-T = input()
-print(f"T: {T}")
 
-for i in T:
+T = int(input())
+
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
+def dfs( i,j, visited):
+    visited[j][i] = True
+    for d in range(4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0<=nx<M and 0<=ny<N:
+            if not visited[ny][nx] and graph[j][i]==1:
+                dfs(nx, ny, visited)
+    
+    
+# 가로: M x i
+# 세로: N y j
+for i in range(T):
     M, N, K = map(int, input().split())
-    print(f"M: {M}, N: {N}, K: {K}")
-    for j in K:
+    graph = [ [0] * M for _ in range(N)]
+    visited = [[False] * M for _ in range(N)]
+    for j in range(K):
         x, y = map(int, input().split())
-        print(f"x: {x}, y: {y}")
+        graph[y][x] = 1
+
+    count = 0
+    
+    for i in range(M):
+        for j in range(N):
+            if not visited[j][i] and graph[j][i]==1:
+                dfs(i,j, visited)
+                count += 1
+                
+    print(count)
