85dd61c99da75f151cad83f7f97124abf7a23329
jhwan <plasticat104@gmail.com>
2025-04-27 15:14:14 +0900

    연결 요소의 개수

--- BOJ/BFS/11724_연결_요소의_개수.py
+++ BOJ/BFS/11724_연결_요소의_개수.py
@@ -1,7 +1,8 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-# 정점, 간선
+sys.setrecursionlimit(10**6)
+
 N, M = map(int, input().split())
 # graph를 구한다
 graph = [[] for _ in range(N+1)]
@@ -25,4 +26,32 @@ for  i in range(N):
                 queue+=graph[v]
 
         count+=1
+print(count)
+
+#& 다른 풀이
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+# dfs 함수
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+count = 0 # 연결 노드의 수
+visited = [False] * (n+1)
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1 # dfs 한 번 끝날 때마다 count+1
+
 print(count)
