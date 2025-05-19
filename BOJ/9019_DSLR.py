fca07b0bea904c5d3eff994fc3eebd98edad4f76
jihwan <jihwan255@baikal.ai>
2025-05-19 13:44:27 +0900

    DSLR

--- BOJ/9019_DSLR.py
+++ BOJ/9019_DSLR.py
@@ -1,5 +1,7 @@
 '''
 BFS로 판단
+문제를 제대로 읽지 않아서 틀렸다.
+L,R에서 10에대해 실행하면 01이 돼서 1이 되는줄 알았는데 100이 돼야한다.
 '''
 
 import sys
@@ -7,22 +9,29 @@ input = sys.stdin.readline
 from collections import deque
 
 T = int(input())
-chk = [False] * 10000
-def D(A):
-    A
-    return
-def S(A):
-    return
-def L(A):
-    return
-def R():
-    return
 
-# '1234'
+def D(A):
+    res = A * 2
+    if res > 9999:
+        return res % 10000
+    return res
+def S(A):
+    if A == 0:
+        return 9999
+    return A - 1
+def L(n):
+    return (n % 1000) * 10 + (n // 1000)
+
+def R(n):
+    return (n % 10) * 1000 + (n // 10)
+
+
 for _ in range(T):
-    A, B =  list(input().split())
+    chk = [False] * 10001
+    A, B =  list(map(int,input().split()))
     Q = deque()
-    Q.append((A, 0))
+    Q.append((A, ''))
+    chk[A] = True
 
     while Q:
         v, count = Q.popleft()
@@ -30,11 +39,27 @@ for _ in range(T):
         s = S(v)
         l = L(v)
         r = R(v)
-
-
-
-
-def BFS():
-    return
-
-print(BFS())
+        if d == B:
+            print(count+'D')
+            break
+        if s == B:
+            print(count+'S')
+            break
+        if l == B:
+            print(count+'L')
+            break
+        if r == B:
+            print(count+'R')
+            break
+        if chk[d] == False:
+            Q.append((d, count+'D'))
+            chk[d] = True
+        if chk[s] == False:
+            Q.append((s, count+'S'))
+            chk[s] = True
+        if chk[l] == False:
+            Q.append((l, count+'L'))
+            chk[l] = True
+        if chk[r] == False:
+            Q.append((r, count+'R'))
+            chk[r] = True
